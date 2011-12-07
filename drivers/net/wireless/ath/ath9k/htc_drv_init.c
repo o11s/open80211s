519645c630018c549a566b2a68d07152e8d11b35
Javier Cardona <javier@cozybit.com>
2011-12-07 11:46:02 -0800

    ath9k_htc: Add support for NL80211_IFTYPE_MESH_POINT interfaces
    More specifically, enable AP-style beaconing on mesh ifaces, honor
    FIF_OTHER_BSS filter and change the hw capabilities to reflect mesh
    support.
    
    Also enable IEEE80211_HW_SUPPORTS_PER_STA_GTK, IEEE80211_HW_MFP_CAPABLE
    and WIPHY_FLAG_IBSS_RSN.  Probably these should depend on the
    capabilities of the hardware but I don't know which hardware supports
    what.  But these are required for secured mesh and work fine on our
    TL-WN821N
    
    Signed-off-by: Javier Cardona <javier@cozybit.com>

--- drivers/net/wireless/ath/ath9k/htc_drv_init.c
+++ drivers/net/wireless/ath/ath9k/htc_drv_init.c
@@ -702,6 +702,8 @@ static void ath9k_set_hw_capab(struct ath9k_htc_priv *priv,
 		IEEE80211_HW_SUPPORTS_PS |
 		IEEE80211_HW_PS_NULLFUNC_STACK |
 		IEEE80211_HW_REPORTS_TX_ACK_STATUS |
+		IEEE80211_HW_SUPPORTS_PER_STA_GTK |
+		IEEE80211_HW_MFP_CAPABLE |
 		IEEE80211_HW_HOST_BROADCAST_PS_BUFFERING;
 
 	hw->wiphy->interface_modes =
@@ -709,9 +711,11 @@ static void ath9k_set_hw_capab(struct ath9k_htc_priv *priv,
 		BIT(NL80211_IFTYPE_ADHOC) |
 		BIT(NL80211_IFTYPE_AP) |
 		BIT(NL80211_IFTYPE_P2P_GO) |
-		BIT(NL80211_IFTYPE_P2P_CLIENT);
+		BIT(NL80211_IFTYPE_P2P_CLIENT) |
+		BIT(NL80211_IFTYPE_MESH_POINT);
 
 	hw->wiphy->flags &= ~WIPHY_FLAG_PS_ON_BY_DEFAULT;
+	hw->wiphy->flags |= WIPHY_FLAG_IBSS_RSN;
 
 	hw->wiphy->flags |= WIPHY_FLAG_IBSS_RSN |
 			    WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL;
