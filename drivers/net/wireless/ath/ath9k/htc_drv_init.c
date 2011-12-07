165d586e6dc605ecb5ea63641b2c097ef758869c
Javier Cardona <javier@cozybit.com>
2011-12-07 11:46:02 -0800

    ath9k_htc: Add support for mesh interfaces
    More specifically, enable AP-style beaconing on mesh ifaces, honor
    FIF_OTHER_BSS filter and change the hw capabilities to reflect mesh
    support.
    
    Coexistence with a virtual STA interface was tested as working fine.
    
    Signed-off-by: Javier Cardona <javier@cozybit.com>

--- drivers/net/wireless/ath/ath9k/htc_drv_init.c
+++ drivers/net/wireless/ath/ath9k/htc_drv_init.c
@@ -728,7 +728,8 @@ static void ath9k_set_hw_capab(struct ath9k_htc_priv *priv,
 		BIT(NL80211_IFTYPE_ADHOC) |
 		BIT(NL80211_IFTYPE_AP) |
 		BIT(NL80211_IFTYPE_P2P_GO) |
-		BIT(NL80211_IFTYPE_P2P_CLIENT);
+		BIT(NL80211_IFTYPE_P2P_CLIENT) |
+		BIT(NL80211_IFTYPE_MESH_POINT);
 
 	hw->wiphy->iface_combinations = &if_comb;
 	hw->wiphy->n_iface_combinations = 1;
