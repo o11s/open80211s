64306d671620723efcb70408c2a498c6735d9813
Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
2012-11-08 10:02:07 +0000

    dmaengine: add cpu_relax() to busy-loop in dma_sync_wait()
    Removal of the busy-loop from dma_sync_wait() is not a trivial
    task so just add cpu_relax() to the loop for now.
    
    Cc: Vinod Koul <vinod.koul@intel.com>
    Cc: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Dan Williams <djbw@fb.com>

--- drivers/dma/dmaengine.c
+++ drivers/dma/dmaengine.c
@@ -266,7 +266,10 @@ enum dma_status dma_sync_wait(struct dma_chan *chan, dma_cookie_t cookie)
 			pr_err("%s: timeout!\n", __func__);
 			return DMA_ERROR;
 		}
-	} while (status == DMA_IN_PROGRESS);
+		if (status != DMA_IN_PROGRESS)
+			break;
+		cpu_relax();
+	} while (1);
 
 	return status;
 }
