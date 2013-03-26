c8c4638d5766bc998d00f5f4b31acdef20d193cd
Andrew Morton <akpm@linux-foundation.org>
2013-03-27 10:24:42 +1100

    mm-hugetlb-include-hugepages-in-meminfo-checkpatch-fixes
    ERROR: code indent should use tabs where possible
    #64: FILE: mm/hugetlb.c:2132:
    +^I^I        ^Inid,$
    
    WARNING: please, no space before tabs
    #64: FILE: mm/hugetlb.c:2132:
    +^I^I        ^Inid,$
    
    total: 1 errors, 1 warnings, 52 lines checked
    
    NOTE: whitespace errors detected, you may wish to use scripts/cleanpatch or
          scripts/cleanfile
    
    ./patches/mm-hugetlb-include-hugepages-in-meminfo.patch has style problems, please review.
    
    If any of these errors are false positives, please report
    them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2129,7 +2129,7 @@ void hugetlb_show_meminfo(void)
 	for_each_node_state(nid, N_MEMORY)
 		for_each_hstate(h)
 			pr_info("Node %d hugepages_total=%u hugepages_free=%u hugepages_surp=%u hugepages_size=%lukB\n",
-		        	nid,
+				nid,
 				h->nr_huge_pages_node[nid],
 				h->free_huge_pages_node[nid],
 				h->surplus_huge_pages_node[nid],
