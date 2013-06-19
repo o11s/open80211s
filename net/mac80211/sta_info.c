bbb7f2b2ee319b85c4f4c13e0253e73d5ec922a2
Thomas Pedersen <thomas@cozybit.com>
2013-06-18 19:40:44 -0700

    mac80211: initialize nonpeer_pm
    XXX: hack! but the context changed upstream anyway, don't
    submit from this branch!
    
    Signed-off-by: Thomas Pedersen <thomas@cozybit.com>

--- net/mac80211/sta_info.c
+++ net/mac80211/sta_info.c
@@ -342,6 +342,7 @@ struct sta_info *sta_info_alloc(struct ieee80211_sub_if_data *sdata,
 	INIT_WORK(&sta->drv_unblock_wk, sta_unblock);
 	INIT_WORK(&sta->ampdu_mlme.work, ieee80211_ba_session_work);
 	mutex_init(&sta->ampdu_mlme.mtx);
+	sta->nonpeer_pm = NL80211_MESH_POWER_ACTIVE;
 
 	memcpy(sta->sta.addr, addr, ETH_ALEN);
 	sta->local = local;
