a248d813e31088be5d200ba993e4cd86c62fdd28
Bob Copeland <me@bobcopeland.com>
2013-07-03 00:03:55 -0400

    mwl8787: add initial fw commands framework
    Signed-off-by: Bob Copeland <me@bobcopeland.com>

--- drivers/net/wireless/mwl8787/fw.h
+++ drivers/net/wireless/mwl8787/fw.h
@@ -3,6 +3,8 @@
  *
  * All rights reserved.
  */
+#include <linux/types.h>
+
 #ifndef _MWL8787_FW_H_
 #define _MWL8787_FW_H_
 
@@ -30,5 +32,80 @@
 #define MGMT_MASK_PROBE_RESP			0x20
 #define MGMT_MASK_BEACON			0x100
 
-/* TODO: supported mwl8787tf commands and events go here */
+#define MWL8787_MAX_MULTICAST_LIST_SIZE		32
+/* FIXME ? */
+#define MWL8787_MAX_BEACON_SIZE			1024
+
+#define MWL8787_ACT_SET				1
+
+struct mwl8787_cmd_header {
+	__le16 id;
+	__le16 size;
+	__le16 seq;
+	__le16 result;
+} __packed;
+
+#define MWL8787_CMD_RESET			0x0003
+struct mwl8787_cmd_reset {
+	struct mwl8787_cmd_header header;
+	__le16 action;
+} __packed;
+
+struct mwl8787_cmd_multicast_addr {
+	struct mwl8787_cmd_header header;
+	__le16 action;
+	__le16 num;
+	u8 mac_list[MWL8787_MAX_MULTICAST_LIST_SIZE][ETH_ALEN];
+} __packed;
+
+struct mwl8787_cmd_radio_ctrl {
+	struct mwl8787_cmd_header header;
+	__le16 action;
+	__le16 control;
+} __packed;
+
+struct mwl8787_cmd_rf_channel {
+	struct mwl8787_cmd_header header;
+	__le16 action;
+	__le16 current_channel;
+	__le16 rftype;
+	u8 reserved[34];
+} __packed;
+
+struct mwl8787_cmd_mac_ctrl {
+	struct mwl8787_cmd_header header;
+	__le16 control;
+	__le16 reserved;
+} __packed;
+
+struct mwl8787_cmd_mac_addr {
+	struct mwl8787_cmd_header header;
+	__le16 action;
+	u8 addr[ETH_ALEN];
+} __packed;
+
+struct mwl8787_cmd_beacon_ctrl {
+	struct mwl8787_cmd_header header;
+	__le16 action;
+	__le16 beacon_enable;
+	__le16 beacon_period;
+} __packed;
+
+struct mwl8787_cmd_beacon_set {
+	struct mwl8787_cmd_header header;
+	__le16 len;
+	u8 beacon[MWL8787_MAX_BEACON_SIZE];
+} __packed;
+
+struct mwl8787_cmd_set_mode {
+	struct mwl8787_cmd_header header;
+	__le16 mode;
+} __packed;
+
+struct mwl8787_cmd_set_bssid {
+	struct mwl8787_cmd_header header;
+	u8 bssid[6];
+	u8 activate;
+} __packed;
+
 #endif /* _MWL8787_FW_H_ */
