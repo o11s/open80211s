6b631630fbee7aa901cb502591d0fa398632ab59
Bob Copeland <me@bobcopeland.com>
2013-10-29 13:06:23 -0400

    mwl8787: implement tbtt event handling
    Signed-off-by: Bob Copeland <me@bobcopeland.com>

--- drivers/net/wireless/mwl8787/event.c
+++ drivers/net/wireless/mwl8787/event.c
@@ -15,6 +15,9 @@ void mwl8787_event_rx(struct mwl8787_priv *priv, struct sk_buff *skb)
 	case MWL8787_EVT_TX_FAIL:
 		mwl8787_tx_fail(priv, event);
 		break;
+	case MWL8787_EVT_PRE_TBTT:
+		mwl8787_beacon_prepare(priv, priv->vif);
+		break;
 	default:
 		dev_err(priv->dev, "Unknown event: %d\n", le16_to_cpu(event->hdr.id));
 		break;
